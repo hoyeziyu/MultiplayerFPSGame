b120ebf6472caa22742bdeddf2108df0f88b2d10
hoyeziyu <yeziyu789@gmail.com>
2025-08-03 13:07:53 +0800

    .uplugin和.cs文件也不采用lfs格式

--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/MultiplayerSessions.Build.cs
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/MultiplayerSessions.Build.cs
@@ -1,3 +1,55 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:47c179de798cbf98ed2ed98d0779dc98ab395c976a3995689f5424d33358fedd
-size 1314
+// Copyright Epic Games, Inc. All Rights Reserved.
+// 指定编译时应该构建哪些模块
+using UnrealBuildTool;
+
+public class MultiplayerSessions : ModuleRules
+{
+	public MultiplayerSessions(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+				// ... add public include paths required here ...
+			}
+			);
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				// ... add other private include paths required here ...
+			}
+			);
+			
+		// 这些模块我们也可以在public文件夹中可用
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"OnlineSubsystem",
+				"OnlineSubsystemSteam",
+				// ... add other public dependencies that you statically link with here ... 
+			}
+			);
+			
+		// 这些模块只在我们的private文件夹中可用
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+				// ... add private dependencies that you statically link with here ...	
+			}
+			);
+		
+		
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+			);
+	}
+}
