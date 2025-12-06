0abde2f1fbaffd15ec66c72cf1d2b2ea2b80edc1
hoyeziyu <yeziyu789@gmail.com>
2025-12-06 22:06:41 +0800

    新增Health Pickup 和相关资源

---
+++ Source/Blaster/Pickups/HealthPickup.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Pickup.h"
+#include "HealthPickup.generated.h"
+
+
+class UNiagaraComponent;
+class UNiagaraSystem;
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API AHealthPickup : public APickup
+{
+	GENERATED_BODY()
+public:
+	AHealthPickup();
+	virtual void Destroyed() override;
+protected:
+	virtual void OnSphereOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	);
+private:
+
+	UPROPERTY(EditAnywhere)
+	float HealAmount = 100.f;
+
+	UPROPERTY(EditAnywhere)
+	float HealingTime = 5.f;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UNiagaraComponent> PickupEffectComponent;
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UNiagaraSystem> PickupEffect;
+};
