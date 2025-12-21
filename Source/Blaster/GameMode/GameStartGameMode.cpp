c3ea114129a63a854cea89caca0221837e40c970
hoyeziyu <yeziyu789@gmail.com>
2025-12-21 20:48:50 +0800

    新增BlasterMap和asset

--- Source/Blaster/GameMode/GameStartGameMode.cpp
+++ Source/Blaster/GameMode/GameStartGameMode.cpp
@@ -7,7 +7,7 @@
 AGameStartGameMode::AGameStartGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_BlasterCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
